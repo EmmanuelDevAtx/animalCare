cf1af0f5614604af90ea8a2b40df59db224e69d4
Astratos <120133182+EmmanuelDevAtx@users.noreply.github.com>
2023-10-28 21:25:44 -0600

    updgrade config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,18 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
+
+const SEPOLIA_PRIVATE_KEY = "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80"
+
+
 const config: HardhatUserConfig = {
-  solidity: "0.8.19",
+  solidity: "0.8.9",
+  networks:{
+    testing:{
+      url:'http://127.0.0.1:8545/',
+      accounts: [SEPOLIA_PRIVATE_KEY]
+    }
+  }
 };
 
 export default config;
--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,26 +1,19 @@
 import { ethers } from "hardhat";
 
+
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
+  const [deployer] = await ethers.getSigners();
 
-  const lockedAmount = ethers.parseEther("0.001");
+  console.log("Deploying contracts with the account:", deployer.address);
 
-  const lock = await ethers.deployContract("Lock", [unlockTime], {
-    value: lockedAmount,
-  });
+  const ethHandle = await ethers.deployContract("ethReciver");
+;
+  console.log(' ethHandle address :', await ethHandle.getAddress())
+  const payments = await ethers.deployContract("AnimalsPayable", [await ethHandle.getAddress()]);
 
-  await lock.waitForDeployment();
-
-  console.log(
-    `Lock with ${ethers.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.target}`
-  );
+  console.log(" Animals payable address : ", await payments.getAddress());
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
 main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
